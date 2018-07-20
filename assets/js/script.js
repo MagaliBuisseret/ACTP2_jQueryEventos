30b6cdd32e51188c6dcdb7d7f075c05539e01b12
Magali <magali.buisseret@gmail.com>
2018-07-19 20:21:37 -0400

    ejercicio 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,5 +5,10 @@ $(document).ready(function() {
     $(".card__like").on("click", function(event){
     	$(this).toggleClass("card__like--red");
     	event.stopPropagation();
+    	//$(this).attr("href", " ")
+    })
+    $(".card__follow-btn").on("click", function(event){
+    	$(this).toggleClass("card__follow-btn--following");
+    	event.stopPropagation();
     })
 });
